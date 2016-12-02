a00975ee228806fd82e55864588c38c933b3de77
alexbruch <alexbruch.cat@gmail.com>
2016-12-02 16:48:51 +0100

    Petits canvis

---
+++ app/src/main/java/com/s6/lasalle/recursos/elementos/NinePatch.java
@@ -0,0 +1,15 @@
+package com.s6.lasalle.recursos.elementos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.s6.lasalle.recursos.R;
+
+public class NinePatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ninepatch);
+    }
+}
