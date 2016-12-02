5470a560bc95d39790ff5e3fc15cc6bd5f10d6bb
alexbruch <alexbruch.cat@gmail.com>
2016-12-02 16:23:08 +0100

    Sessió 6

--- app/src/main/java/com/s6/lasalle/recursos/elementos/Bitmap_r.java
+++ app/src/main/java/com/s6/lasalle/recursos/elementos/Bitmap_r.java
@@ -1,9 +1,13 @@
 package com.s6.lasalle.recursos.elementos;
 
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.s6.lasalle.recursos.R;
@@ -15,8 +19,36 @@ public class Bitmap_r extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bitmap);
 
+        /** BITMAP **/
+
         ImageView image = (ImageView) findViewById(R.id.imageView);
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.bcn);
         image.setImageBitmap(bitmap);
+
+        /** BUTTON **/
+
+        Button button = (Button) findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Bitmap_r.this);
+                builder.setMessage(getString(R.string.alert_dialog_details)).setTitle(getString(R.string.alert_dialog_title));
+                builder.setPositiveButton(getString(R.string.accept), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+                builder.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
     }
 }
