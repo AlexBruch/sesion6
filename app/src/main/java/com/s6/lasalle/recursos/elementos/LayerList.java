8f305217972a8039eca0c94f1ca5f93873a9eed5
alexbruch <alexbruch.cat@gmail.com>
2016-12-02 18:41:48 +0100

    Layer list OK

--- app/src/main/java/com/s6/lasalle/recursos/elementos/LayerList.java
+++ app/src/main/java/com/s6/lasalle/recursos/elementos/LayerList.java
@@ -1,5 +1,9 @@
 package com.s6.lasalle.recursos.elementos;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +15,12 @@ public class LayerList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layerlist);
+
+        Drawable d = (Drawable) getResources().getDrawable(R.drawable.layerlist);
+        Bitmap b = Bitmap.createBitmap(d.getIntrinsicWidth(), d.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+
+        LayerDrawable ld = (LayerDrawable) getResources().getDrawable(R.drawable.layerlist);
+        ld.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
+        ld.draw(new Canvas(b));
     }
 }
